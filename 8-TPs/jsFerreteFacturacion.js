c86c1c39d66ffd9debfc73c2f39278f6a9c4e406
aropervi <60695334+aropervi@users.noreply.github.com>
2020-02-13 12:32:28 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,70 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+var precio1;
+
+var precio2;
+
+var precio3;
+
+var resultado;
+
+precio1 = parseFloat(document.getElementById("PrecioUno").value);
+
+precio2 = parseFloat(document.getElementById("PrecioDos").value);
+
+precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+resultado = precio1 + precio2 + precio3;
+
+alert ("la suma de los mismos es " + resultado);
+
+
+
 }
 function Promedio () 
 {
-	
+    var precio1;
+
+    var precio2;
+    
+    var precio3;
+    
+    var resultado;
+    
+    precio1 = parseFloat(document.getElementById("PrecioUno").value);
+    
+    precio2 = parseFloat(document.getElementById("PrecioDos").value);
+    
+    precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+    resultado = (precio1 + precio2 + precio3) / 3;
+    
+    alert ("el promedio es " + resultado);
+
 }
 function PrecioFinal () 
 {
-	
+	var precio1;
+
+    var precio2;
+    
+    var precio3;
+    
+    var resultado1;
+
+    var resultado2;
+    
+    precio1 = parseFloat(document.getElementById("PrecioUno").value);
+    
+    precio2 = parseFloat(document.getElementById("PrecioDos").value);
+    
+    precio3 = parseFloat(document.getElementById("PrecioTres").value);
+
+    resultado1 = (((precio1 + precio2 + precio3) * 21) / 100);
+    
+    resultado2 = resultado1 + (precio1 + precio2 + precio3); 
+
+    alert ("El importe total más el IVA es del " + resultado2);
+
 }
