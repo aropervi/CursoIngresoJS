33fd938ade08c95e80f201ddad5b5880fee23a2a
aropervi <60695334+aropervi@users.noreply.github.com>
2020-02-27 12:23:15 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,58 @@
 function mostrar()
 {
+var letra;
+var numero;
+var numerosPar= 0;
+var numerosImp= 0;
+var cantidadCeros= 0;
+var promedioPos;
+var sumaPos= 0;
+var sumaNeg = 0;
+var MaximoL;
+var MaximoN;
+var minimL;
+var minimN;
+var seguir;
+(numero>-100 && numero<-100)
+
+
+do { numero= parseInt(prompt("Ingrese un numero"));
+
+while (isNaN(numero) || numero>100 || numero<-100){
+
+numero= parseInt(prompt("Ese no es un numero valido por favor reingreselo"));
+
+}
+if ((numero % 2)= 0) {
+numerosPar ++;
+}
+
+else if (numero = 0){
+
+cantidadCeros ++;
+}
+else {
+numerosImp ++; 
+}
+
+if (numero<0){
+sumaNeg ++;    
+} 
+else {
+sumaPos ++;    
+}
+
+letra = prompt("Ingrese una letra");
+while (letra < "a" || letra > "z" || letra < "A" || letra > "Z" ){
+    letra = prompt("La letra ingresada no es valida, por favor reingresela");
+}
+
+seguir = prompt("Desea continuar el ingreso? Ingrese `s` o `n`");
+
+} 
+
+while (seguir == s)
+
+document.write("Cantidad de numeros pares " + numerosPar + "<\br> ");
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+var notas;
+var sexo;
+var acumuladorNotas =0;
+var promedioNotas;
+var notaBaja;
+var SexoBajo;
+var flag = 0;
+var contadorVaronesMas6= 0; //recorda inicializar cada cosa que te pidan en el problema
+
+for(i=0;i<5;i++){
+
+notas= parseInt(prompt("ingrese la nota"));
+while (isNaN(notas) || (notas<0) && (notas>10)) {
+    notas = parseInt(prompt("Nota invalida, por favor reingrese"));
+} 
+    sexo= prompt("Ingrese el sexo del alumno");
+
+while (sexo!== "m" && sexo!=="f" ){
+
+sexo= prompt("Sexo invalido, por favor reingrese"); 
+}
+}
+
+acumuladorNotas = acumuladorNotas + notas;
+
+if (notas<notaBaja || flag==0) {
+notaBaja = notas;
+flag=1;
+SexoBajo = sexo
+}
+
+else if (sexo== m && nota >6){
+
+contadorVaronesMas6 ++;
 
 }
+alert ("Promedio = " + acumuladorNotas / 5 + "\n Nota mas baja = " + notaBaja + SexoBajo + "\n Cantidad de varones con nota menor a 6 = " + contadorVaronesMas6);
+
+
+}
+
+
+
+
+
